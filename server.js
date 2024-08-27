494392596a4b30f66efe4bad5ef0c72f7c8fa218
ReziMi <revaz.mikadze92@gmail.com>
2024-08-28 02:12:19 +0400

    env, postman, router

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 import http from 'http';
-const PORT = 8000;
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 const server = http.createServer((req,res) => {
     // res.write("Hello World!");
     // res.setHeader('Content-type', "text/html");
     // res.setHeader('Content-type', "text/plain");
     // res.statusCode = 404;
-    res.writeHead(500, {"Content-type": 'application/json'})
-    res.end(JSON.stringify({message: "Server error"}));
+    // console.log(req.url);
+    // console.log(req.method);
+
+    if(req.url==='/'){
+        res.writeHead(200, {"Content-type": 'text/html'})
+        res.end('<h1>Homepage/h1>');
+    }else if(req.url==="/about"){
+        res.writeHead(200, {"Content-type": 'text/html'})
+        res.end('<h1>Aboutpage/h1>'); 
+    }else{
+        res.writeHead(404, {"Content-type": 'text/html'})
+        res.end('<h1>not found/h1>');
+    }
 });
 
 server.listen(PORT, ()=>{
