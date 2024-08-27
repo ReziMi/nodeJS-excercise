c0a3405aa7aa80b4a9f7999b96cf51028559839b
ReziMi <revaz.mikadze92@gmail.com>
2024-08-28 01:06:43 +0400

    CommonJs and ES modules

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello.")
+// console.log("Hello, world");
+
+// const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+// console.log(`Celcius to Fahrenhiet: ${celciusToFahrenheit(20)}`);
+
+import getPost, {getPostLength} from "./postController.js";
+console.log(getPost());
+console.log(getPostLength());
